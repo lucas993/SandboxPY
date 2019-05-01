cabc115a6e16150bbaad7283d060ad6d7032b598
s7ae1k3r <34325950+s7ae1k3r@users.noreply.github.com>
2019-04-30 22:21:55 -0400

    Update solution.py
    only need one method, i forgot the other "n"s

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 # Forcing recursion for no good reason.  But it passed so....
-
-def solution_r(n):
-    if n <= 0:
-        return n
-    else:
-        if not n%3 or not n%5:
-            return n + solution_r(n-1)
-        else:
-            return solution_r(n-1)
-
 def solution(number):
     if not number:
         return 0
-    return solution_r(number-1)
+    
+    if number <= 0:
+        return number
+    else:
+        if not number%3 or not number%5:
+            return number + solution_r(number-1)
+        else:
+            return solution(number-1)
+    return solution(number-1)
 
-assert solution(10) == 23, "Oops, recursion is the devil"
+assert(solution(10) == 23, "Yay")
