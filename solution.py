65e7beb2da6513aa3d834675a88b41446d2aa5d5
lucas993 <lucas993@users.noreply.github.com>
2019-04-30 22:17:35 -0400

    adding a solution using recursion

---
+++ solution.py
@@ -0,0 +1,17 @@
+# Forcing recursion for no good reason.  But it passed so....
+
+def solution_r(n):
+    if n <= 0:
+        return n
+    else:
+        if not n%3 or not n%5:
+            return n + solution_r(n-1)
+        else:
+            return solution_r(n-1)
+
+def solution(number):
+    if not number:
+        return 0
+    return solution_r(number-1)
+
+assert(solution(10) == 23, "Yay")
